42ef5d516ee5ffc19e47b66184b70dced04918b1
Sonabayim <huseynzade.sonabeyim@gmail.com>
2019-05-17 14:59:51 +0200

    nothing importnt

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+	name	= models.CharField(max_length=120)
+	author	= models.CharField(max_length=120)
+	category= models.CharField(max_length=120, null=True, blank=True)
+	b_file	= models.TextField()
+
+
+	def __str__(self):
+		return self.name
--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+# from django.views.generic import View
+from django.views import View
+
 
 # Create your views here.
+class HomeView(View):
+	def get(self, request, *args, **kwargs):
+		context = {}
+		return render(request, "home.html", context)

44461a4b22ebc0e403f60650ad87dc748494d183
Sonabayim <huseynzade.sonabeyim@gmail.com>
2019-05-24 18:49:29 +0200

    trtyy

--- xan/settings/production.py
+++ xan/settings/production.py
@@ -131,24 +131,24 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static-storage"),
-]
-
-STATIC_ROOT = 'var/www/static-root/'
-
-MEDIA_URL = 'media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media", "/var/www/media-root/")
-
 # STATIC_URL = '/static/'
+
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static-storage"),
 # ]
 
+# STATIC_ROOT = 'var/www/static-root/'
 
-# STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
-# MEDIA_URL   = '/media/'
-# MEDIA_ROOT  = os.path.join(os.path.dirname(BASE_DIR), "media-serve")
+# MEDIA_URL = 'media/'
+
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media", "/var/www/media-root/")
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
+MEDIA_URL   = 'media/'
+MEDIA_ROOT  = os.path.join(os.path.dirname(BASE_DIR), "media-serve")

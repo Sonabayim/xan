8ddee29478af8cdae7d8da410545a4c35f23a73e
Sonabayim <huseynzade.sonabeyim@gmail.com>
2019-05-24 18:11:06 +0200

    changes

--- xan/settings/production.py
+++ xan/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = 'uxm&_1i33lz8$h%=!3&(-20bnt6=hux5n!p#n=@)9i6ql(qs^7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'books',
 ]
 
 MIDDLEWARE = [
@@ -80,7 +81,16 @@ DATABASES = {
     }
 }
 
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'nicechodb',
+#         'USER': 'nice',
+#         'PASSWORD': 'nicecho0205',
+#         'HOST': 'localhost',
+#         'PORT': '',
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
 
@@ -118,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static-storage"),
+]
+
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static-serve")
+MEDIA_URL   = '/media/'
+MEDIA_ROOT  = os.path.join(os.path.dirname(BASE_DIR), "media-serve")

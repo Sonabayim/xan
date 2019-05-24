3e33b30a2169000e22a14310b52a46b5bf170a1e
Sonabayim <huseynzade.sonabeyim@gmail.com>
2019-05-24 18:33:16 +0200

    try3

--- xan/settings/production.py
+++ xan/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY','uxm&_1i33lz8$h%=!3&(-20bnt6=hux5n!p#n=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['xanplusplus.herokuapp.com']
 
 
 # Application definition
